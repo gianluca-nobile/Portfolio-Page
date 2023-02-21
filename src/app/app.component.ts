5ccae185a9f34b63662fa7c38d126060e7f314bb
Gianlu <nobile.gianluca@gmail.com>
2023-02-22 00:06:19 +0100

    add component (home about loader)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,40 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
+
   title = 'gianlucaPage';
 
-  mode = 'light'
+  loading = false;
+  tab = 'home';
+  mode = 'dark';
+
+
+  ngOnInit(): void {
+      this.loading = true
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.loading = false;
+    }, 500)
+  }
 
   switchMode() {
     const html = document.documentElement;
       html.classList.toggle('dark');
-
     if (this.mode === 'light'){
       this.mode = 'dark';
     }else {
       this.mode = 'light';
     }
-
   }
+  switchTab(tab: string){
+    this.tab = tab;
+  }
+
 }
