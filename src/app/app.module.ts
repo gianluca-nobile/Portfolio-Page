8149c3c8890cc5b740a7b62dfb2536c739259ec5
Gianlu <nobile.gianluca@gmail.com>
2023-02-22 00:06:41 +0100

    add component (home about loader)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { AboutComponent } from './page/about/about.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
